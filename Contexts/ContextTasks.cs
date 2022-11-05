fa846a66a1f9e186d7d001133889f5f8e31dde00
Juan Andrade <jpas.juan01@gmail.com>
2022-11-04 23:49:40 -0500

    I tested a database in memory.

--- Contexts/ContextTasks.cs
+++ Contexts/ContextTasks.cs
@@ -1,13 +1,12 @@
 using Microsoft.EntityFrameworkCore;
 using proj_tareas_categ.Models;
-using Task = proj_tareas_categ.Models.Task;
 
 namespace proj_tareas_categ.Contexts
 {
     public class ContextTasks : DbContext
     {
         public DbSet<Category> Categories { get; set; }
-        public DbSet<Task> Tasks { get; set; }
+        public DbSet<Models.Task> Tasks { get; set; }
         public ContextTasks(DbContextOptions<ContextTasks> options) : base(options) { }
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,6 +1,20 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using proj_tareas_categ.Contexts;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<ContextTasks>(p => p.UseInMemoryDatabase("TasksDB"));
+
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
+
+app.MapGet("/dbconection", async ([FromServices] ContextTasks dbContext) =>
+{
+    dbContext.Database.EnsureCreated();
+
+    return Results.Ok("Base de datos en memoria: " + dbContext.Database.IsInMemory());
+});
 
 app.Run();
