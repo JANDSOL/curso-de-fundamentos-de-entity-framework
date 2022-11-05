867aa8feb07507dbeb0e544024401a639a682cc9
Juan Andrade <jpas.juan01@gmail.com>
2022-11-04 22:40:34 -0500

    I update two files:  1. Category.cs  2. Task.cs where I mapped models using attributes.

--- Models/Category.cs
+++ Models/Category.cs
@@ -1,9 +1,20 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace proj_tareas_categ.Models
 {
     public class Category
     {
+        [Key]
         public Guid CategoryId { get; set; }
+
+        [
+            Required(ErrorMessage = "Ingresa un nombre para la tarea"),
+            MinLength(2, ErrorMessage = "Ingresa un nombre de tarea válido"),
+            MaxLength(150, ErrorMessage = "¡Superaste el límite de caracteres!")
+        ]
         public string Name { get; set; }
+
+        [MaxLength(500, ErrorMessage = "¡Superaste el límite de caracteres!")]
         public string Description { get; set; }
 
         public virtual ICollection<Task> Tasks { get; set; }
--- Models/Task.cs
+++ Models/Task.cs
@@ -1,16 +1,39 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using proj_tareas_categ.Utils;
 
 namespace proj_tareas_categ.Models
 {
     public class Task
     {
+        [Key]
         public Guid TaskId { get; set; }
+
+        [ForeignKey("CategoryId")]
         public Guid CategoryId { get; set; }
+
+        [
+            Required(ErrorMessage = "Ingresa un título para la categoria"),
+            MinLength(2, ErrorMessage = "Ingresa un título de categoria válido"),
+            MaxLength(200, ErrorMessage = "¡Superaste el límite de caracteres!")
+        ]
         public string Title { get; set; }
+
+        [MaxLength(500, ErrorMessage = "¡Superaste el límite de caracteres!")]
         public string Description { get; set; }
-        public Priority PriorityTask { get; set; }
-        public DateTime CreationDate { get; set; }
+
+        public Priority PriorityTask { get; set; } = Priority.Low;
+
+        [DataType(DataType.DateTime)]
+        [DisplayFormat(
+            ApplyFormatInEditMode = true,
+            DataFormatString = "{g}")
+        ]
+        public DateTime CreationDate { get; set; } = DateTime.Now;
 
         public virtual Category Category { get; set; }
+
+        [NotMapped]
+        public string Summary { get; set; }
     }
 }
