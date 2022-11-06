2d77df691a388f9bde376e34b9ba0d077a172ae8
Juan Andrade <jpas.juan01@gmail.com>
2022-11-06 17:39:19 -0500

    I implement Put and Delete methods and finish the course.

--- Program.cs
+++ Program.cs
@@ -21,13 +21,7 @@ app.MapGet("/dbconection", async ([FromServices] ContextTasks dbContext) =>
 
 app.MapGet("/api/get/tareas", async ([FromServices] ContextTasks dbContext) =>
 {
-    return Results.Ok(
-        dbContext.Tasks
-            .Include(p => p.Category)
-            .Where(p => p.PriorityTask == Priority.Low
-                || p.PriorityTask == Priority.High
-            )
-    );
+    return Results.Ok(dbContext.Tasks.Include(p => p.Category));
 });
 
 app.MapPost("/api/post/tareas", async ([FromServices] ContextTasks dbContext, [FromBody] Task task) =>
@@ -37,6 +31,43 @@ app.MapPost("/api/post/tareas", async ([FromServices] ContextTasks dbContext, [F
     await dbContext.SaveChangesAsync();
 
     return Results.Ok();
+});
+
+app.MapPut("/api/put/tarea/{id}", async ([FromServices] ContextTasks dbContext, [FromBody] Task task, [FromRoute] Guid id) =>
+{
+    var currentTask = dbContext.Tasks.Find(id);
+
+    if (currentTask != null)
+    {
+        currentTask.CategoryId = task.CategoryId;
+        currentTask.Title = task.Title;
+        currentTask.Description = task.Description;
+        currentTask.PriorityTask = task.PriorityTask;
+        currentTask.Status = task.Status;
+
+        await dbContext.SaveChangesAsync();
+
+        return Results.Ok();
+    }
+
+    return Results.NotFound();
+
+});
+
+app.MapDelete("/api/delete/tarea={id}", async ([FromServices] ContextTasks dbContext, [FromRoute] Guid id) =>
+{
+    var currentTask = dbContext.Tasks.Find(id);
+
+    if (currentTask != null)
+    {
+        dbContext.Remove(currentTask);
+        await dbContext.SaveChangesAsync();
+
+        return Results.Ok();
+    }
+
+    return Results.NotFound();
+
 });
 
 app.Run();
